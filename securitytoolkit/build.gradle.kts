097b61ec1b16574faadaea9e5c45b38537cf5a26
Denis Dobanda <denis.dobanda@exxeta.com>
2024-10-23 14:20:31 +0200

    feat: configure gradle versions for jitpack
    Signed-off-by: Denis Dobanda <denis.dobanda@exxeta.com>

--- securitytoolkit/build.gradle.kts
+++ securitytoolkit/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    libs.plugins.android.library
-    libs.plugins.jetbrains.kotlin.android
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.jetbrains.kotlin.android)
     id("maven-publish")
     signing
 }
